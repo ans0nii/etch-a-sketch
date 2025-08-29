b20d0c77c0e6fb780e95020cf023e0c6d9d10ce6
Anthony <anthonys2u2@gmail.com>
2025-08-29 16:20:12 -0500

    Add hover listener and limit click eventListener

--- etch.js
+++ etch.js
@@ -1,20 +1,26 @@
+let clickCount = 0;
 document.querySelectorAll(".dimensions button").forEach((element) => {
-  element.addEventListener("click", (e) => {
-    let [a, b] = e.target.textContent.split("x").map(Number);
-    let totalBoxes = a * b;
-    requestedBoxes(totalBoxes);
+    element.addEventListener("click", (e) => {
+        if (clickCount >= 3) return;
+        clickCount++;
+        let [a, b] = e.target.textContent.split("x").map(Number);
+        let totalBoxes = a * b;
+        requestedBoxes(totalBoxes);
     console.log(totalBoxes);
-  });
+});
 });
 let requestedBoxes = (totalBoxes) => {
-  let gridSize = Math.sqrt(totalBoxes);
-
-  let containerSize = `calc(${100 / gridSize}% - 2px)`;
-  for (let i = 0; i < totalBoxes; i++) {
-    const box = document.createElement("div");
-    box.classList.add("box");
-    box.style.width = containerSize;
-    box.style.height = "30px";
-    document.querySelector(".container").appendChild(box);
+    let gridSize = Math.sqrt(totalBoxes);
+    
+    let containerSize = `calc(${100 / gridSize}% - 2px)`;
+    for (let i = 0; i < totalBoxes; i++) {
+        const box = document.createElement("div");
+        box.classList.add("box");
+        box.style.width = containerSize;
+        box.style.height = "30px";
+        document.querySelector(".container").appendChild(box);
+        box.addEventListener("mouseover", (e) => {    
+            e.target.style.backgroundColor = "grey";
+    });
   }
 };
